f86791ed5cd05fd5023e5c1960deaea2cab26c71
Yoni190 <yfisuhu@gmail.com>
2024-08-06 08:46:19 +0300

    Allow user to set size
    The user can provide a number and then the size of the blocks is calculated by dividing the height(width) by the size provided.

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 const container = document.querySelector("#container");
+const change = document.querySelector("button");
 
-for(let i = 0; i<256; i++){
-    const temp = document.createElement("div");
-    temp.style.border = "2px dotted black";
-    temp.style.height = "20px";
-    temp.style.width = "20.3px";
-    temp.addEventListener("mouseover", ()=>{
-        temp.style.backgroundColor = "green";
-    });
-    container.appendChild(temp);
+
+let gridSize = 16;
+
+change.addEventListener("click", ()=>{
+    let size = prompt("Enter a size from 1-100");
+    if(size>100){
+        alert("100 is the limit");
+    }
+    else{
+        gridSize = size;
+        while(container.firstChild){
+            container.removeChild(container.firstChild);
+        }
+        createGrid(gridSize);
+    }
+});
+
+function createGrid(gridSize){
+    blockHeight = 400/gridSize;
+    blockWidth = 400/gridSize;
+    for(let i = 0; i< (gridSize * gridSize); i++){
+        const temp = document.createElement("div");
+        temp.className = "block";
+        temp.style.border = "1px solid black";
+        temp.style.height = blockHeight +"px";
+        temp.style.width = blockWidth +"px";
+        temp.addEventListener("mouseover", ()=>{
+            temp.style.backgroundColor = "green";
+        });
+        container.appendChild(temp);
+    }
 }
+createGrid(gridSize);
